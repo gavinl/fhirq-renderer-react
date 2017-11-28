6e45327a7995d2064e798e48b8771b1a68b39880
Gavin Lang <gavinlang@protonmail.com>
2017-11-29 10:28:53 +1100

    fix script attempt #1

--- tools/deploy/before_deploy.sh
+++ tools/deploy/before_deploy.sh
@@ -1,7 +1,5 @@
-#!/bin/sh
-
 cat .gitignore | grep -v ^dist$ > temp.gitignore
 mv temp.gitignore .gitignore
-rm -rf node_modules
+rm -rf node_modules coverage
 
 cp tools/deploy/web.config.sample dist/web.config
